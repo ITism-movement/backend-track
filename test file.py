67326bea19e7fa6d39df4a41e6089be134e32e3c
maritimiss <maritimiss@gmail.com>
2024-12-05 17:00:49 +0100

    commit via terminal, added prompt

--- test file.py
+++
@@ -1,4 +0,0 @@
-a = 10
-b = 10
-sum_ab = a + b
-print(sum)
---
+++ test_file.py
@@ -0,0 +1,4 @@
+a = input("Please enter first number: ")
+b = input("Please enter second number: ")
+sum_ab = a + b
+print("The sum of a and b is", sum_ab)
