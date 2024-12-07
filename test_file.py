19491c5992f97fdd95f162ee17652d901a57f8cd
Ilia <i.a.ivanenko77@gmail.com>
2024-12-07 14:48:12 +0300

    Add comment

--- test_file.py
+++ test_file.py
@@ -6,4 +6,6 @@ a = input("Please enter first number: ")
 b = input("Please enter second number: ")
 sum_ab = a + b
 #commented by Ilia
+#321 comment by Lera
+# This is Zarina's comment
 print("a and b is", sum_ab)
