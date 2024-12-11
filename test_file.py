46dab509f22ba654d7fbda249947e4a3fc937361
maritimiss <maritimiss@gmail.com>
2024-12-11 18:02:08 +0100

    Added a function

--- test_file.py
+++ test_file.py
@@ -2,29 +2,34 @@
 # for i in range(4):
 #     print(''.join(itism))
 # print('The end.')
-# a = input("Please enter first number: ")
-# b = input("Please enter second number: ")
+# a = int(input("Please enter first number: "))
+# b = int(input("Please enter second number: "))
 # sum_ab = a + b
 # #commented by Ilia
 # #321 comment by Lera
 # # This is Zarina's comment
 # # 123 commented by Ivan
-# print("a and b is", sum_ab)
+# print("The sum of the numbers is", sum_ab)
 
 
 a = 1
 # print(type(a))
 
 
-def defined(first=1000, second=1):
-    return first + second
+def defined(func, first=1000, second=1):
+    return func(first, second)
 
+def multiplier(a, b):
+    return a * b
+
+print(defined(multiplier,10,10))
 # res = defined()
 # print(res)
 
 
-print(defined)
-f = defined
-b = defined
-print(b, f)
-print(b(first=100), f(first=200))
+# print(defined)
+# f = defined
+# b = defined
+# print(b, f)
+# print(b(first=100), f(first=200))
+
