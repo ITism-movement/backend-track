66ec3e05be98bbd974b21dfde3b7806da2b90bca
maritimiss <maritimiss@gmail.com>
2024-12-17 20:00:23 +0100

    Внесла правки

--- decorator_practice.py
+++ decorator_practice.py
@@ -1,8 +1,8 @@
-# Декоратор 1
+# Декоратор 1 Оповещение о новом заказе с использованием аргументов функции
 def tg_message(func):
     def wrapper(*args, **kwargs):
         discounted_price = args[2]*0.7
-        print(f"Добрый день, {args[0]}! Вы сделали новый заказ в Ешки Матрешки. Номер вашего заказа {args[1]}. "
+        print(f"Добрый день, {args[0]}! Спасибо за заказ в Ешки Матрешки. Номер вашего заказа {args[1]}. "
               f"Цена со скидкой 30% - {discounted_price} рублей.")
         return func(*args, **kwargs)
     return wrapper
@@ -15,6 +15,7 @@ print(send_message("Антон М.", 1083, 1500))
 
 
 # Декоратор 2
+# Случай, когда необходимо использовать декоратор без "сахара"
 def discount(func):
     def wrapper(*args, **kwargs):
         discounted_price = func(*args, **kwargs)*0.7
@@ -27,7 +28,7 @@ def new_order(cost, markup):
 print(f"Цена без скидки {new_order(50,50)}. Цена со скидкой {discount(new_order)(50,50)}.")
 
 
-# Декоратор 3
+# Декоратор 3 Рамка вокруг приветствия
 def frame(func):
     def wrapper(*args):
         name = f"*{func(*args)}*"
